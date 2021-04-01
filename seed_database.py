953fa8a1e420fbe005a37c86b06bb083e2c4ff46
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-04-01 23:34:20 +0000

    Create test books data. Generate fake users with random ratings.

--- seed_database.py
+++ seed_database.py
@@ -2,6 +2,9 @@
     TODO: pull in media info via json for database."""
 
 import os
+import json
+from faker import Faker
+from random import choice, randint
 
 import crud
 import model
@@ -13,18 +16,41 @@ os.system('createdb library')
 model.connect_to_db(server.app, echo=False)
 model.db.create_all()
 
-# create test users:
-bob = crud.create_user(fname='bob', lname='bobson', email='bob@bob.com', pwd='bob')
+fake = Faker()
 
 # create test data:
 for new_type in ['book', 'movie', 'tv']:
     crud.create_media_type(new_type)
+
+with open('data/test_books.json') as f:
+    book_data = json.loads(f.read())
+books_in_db = []
+for book in book_data['entries']:
+    title, cover_image = (book['title'], book['picture']['url'])
+    new_book = crud.create_book(title=title, type_id=1, author='somebody', cover=cover_image)
+    books_in_db.append(new_book)
 
 cosmos = crud.create_item(title='Cosmos', type_id=1)
 contact = crud.create_book(title='Contact', type_id=1, author='Carl Sagan', pages=434)
 sunshine = crud.create_movie(title='Sunshine', type_id=2, length=107, year=2007)
 sg1_ep1 = crud.create_tv_ep(title='Children of the Gods', type_id=3, 
                         show_title='Stargate SG-1', ep_length=92, season=1)
+
+sources = ['owned', 'library', 'amazon']
+
+# create test users:
+bob = crud.create_user(fname='bob', lname='bobson', email='bob@bob.com', pwd='bob')
+for i in range(10):
+    new_user = crud.create_user(fname=fake.first_name(), 
+                                lname=fake.last_name(), 
+                                email=fake.email(), 
+                                pwd=fake.password())
+    for j in range(5):
+        crud.store_media_in_user_library(user=new_user, 
+                                        media_item=choice(books_in_db), 
+                                        rating=randint(1,5), 
+                                        review=fake.text(), 
+                                        source=choice(sources))
 
 # create & assign genres:
 sci = crud.create_genre(genre_name = 'Science')
