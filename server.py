adfeacac8f72794e779628048ea260d5e5fefcfd
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-04-01 20:22:37 +0000

    Get server running with very basic homepage.

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-"""Server for library app."""
+""" Server for library app. """
 
 from flask import (Flask, render_template, request, flash, session, redirect)
 
 from model import connect_to_db
 
-# import crud
+import crud
 
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def show_homepage():
+    """ Show the homepage. """
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
